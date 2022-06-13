7eef9452de31335212b19daf5cf7f85ba25d62db
ejomo <ejlalmohammed3@gmail.com>
2022-06-13 11:45:25 +0300

    add student models and controller

---
+++ WebAppApi/Controllers/StudentController.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using WebAppApi.Models;
+
+namespace WebAppApi.Controllers
+{
+    [Route("api/student")]
+    [ApiController]
+    public class StudentController : ControllerBase
+    {  
+        public static List<Student> Students = new List<Student>()
+        {
+             new Student(){StudentId=10, Name="EJLAL",level="1"},
+                new Student(){StudentId=20, Name="OLA",level="1"},
+        };
+
+        
+        [HttpGet]
+        public List<Student> GetAll()
+        {
+            return Students;
+        }
+
+        [HttpGet ("{Id}")]
+        public IActionResult GetStudentById(int Id)
+        {
+            var CurStudent = Students.Where(x => x.StudentId == Id).FirstOrDefault();
+            if (CurStudent == null)
+            {
+                return NotFound();
+            }
+            return Ok (CurStudent);
+                
+        }
+    }
+
+}
---
+++ WebAppApi/Models/Student.cs
@@ -0,0 +1,11 @@
+﻿namespace WebAppApi.Models
+{
+    public class Student
+    {  
+        public int StudentId { get; set; }
+        public string Name { get; set; }
+        public string level { get; set; }
+
+
+    }
+}
